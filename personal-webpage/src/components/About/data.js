5d07244b3aaeb4c7c16540020b1b2ce4b9e1a96e
Michellelo123 <michellelo@missionreadyhq.com>
2026-06-01 20:43:41 +1200

    adjusting the information in work and about data

--- personal-webpage/src/components/About/data.js
+++ personal-webpage/src/components/About/data.js
@@ -8,16 +8,16 @@ export const aboutMe = [
     },{
         title: "Why Software Development?",
         description: "I have spent almost 9 years as an Oral Health Therapist — and while I enjoyed the job, there came a point where I thought to myself, can I still see myself here 10 or 20 years down the line?",
-        subDescription: "In the last couple of years I mainly worked as a Dental Hygienist. The treatment I provided to my patients regardless of their age, state of their gum health or the number of teeth. 98% of the time, the process was exactly the same. I did this every work day for every patient. It became repetitive and I needed change. So I made the leap into tech.",
+        subDescription: "In the last couple of years I mainly worked as a Dental Hygienist. The treatment I provided to my patients regardless of their age, state of their gum health or the number of teeth. 98% of the time, the process was exactly the same. I did this every work day for every patient. It became repetitive and I needed change. So I made the decision of studying software development.",
         image: "IMG_4326.PNG"
     },{
         title: "Continuing... Why Software Development?",
         description: "What drew me to software development is that writing lines of code could create pretty much anything. Webpages, applications, games and more. Sometimes when I am working on a project, time flies.",
-        subDescription: "I'm a visual person at heart, which is why I'm naturally drawn more to frontend work. I love the intersection of design and functionality — where things not only work well but feel good to use. Working inside the mouth with small tools and teeth have made me pretty nit-picky about the details, which as it turns out, is a great trait to have as a developer.",
+        subDescription: "I'm a visual person at heart, which is why I'm naturally drawn more to frontend work. I love the intersection of design and functionality — where things not only work well but feel good to use. Working inside a small mouth with small tools and teeth have made me pretty nit-picky about the details, which as it turns out, is a great trait to have as a developer.",
         image: "dentalImg.jpg"
     },{
         title: "When I'm not coding/studying?",
-        description: "I like to go on mini hikes or to the beach with my dogs. I wouldn't say I'm an expert in baking but I make pretty good chocolate chip cookies 🍪, and of course I bake dog biscuits, muffins and cake 🧁 (which they demolish and cry for) ",
-        subDescription:"I also enjoy watching anime, dramas and playing League Of Legends. My usual go to TV series would be The Big Bang Theory. I also occasionally do pet sitting and volunteering. My friends would say I'm funny? (my punny jokes) approachable, kind, and always willing to learn — and I think that sums me up. I'm at the beginning of my tech journey, I understand I have much to learn which I am taking one step at a time.I'm not only learning to code but learning the underlying fundamentals of development. What I do know is that I want to be part of a team where I can learn from colleagues/seniors, collaborate and contribute.",
+        description: "I like to go on mini hikes or to the beach with my dogs. I wouldn't say I'm an expert in baking but I make pretty good chocolate chip cookies 🍪, and of course I like to bake dog biscuits, muffins and cake 🧁 (which my boys demolish and cry for) ",
+        subDescription:"I also enjoy watching anime, dramas and playing League Of Legends. My usual go to TV series would be The Big Bang Theory. I also occasionally do pet sitting and volunteering. How would people describe me? My friends would say I am funny? approachable, kind, and always willing to learn — and I think that sums me up. I am at the beginning of my tech journey. I understand I have much to learn which I am taking one step at a time. What I do know is that I want to be part of a team where I can learn from colleagues/seniors, collaborate and contribute.",
         image: "SLP01230.JPG"
     }]  
